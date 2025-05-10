3817e4a9d9a0cc0306f25296d9be08d6a6711fd1
Jenish Patel <jpatel102005@gmail.com>
2025-05-10 13:36:09 -0400

    decided to use median flow

--- detection.py
+++ detection.py
@@ -2,3 +2,15 @@
 Created on 5-10-2025
 @author: Jenish
 """
+
+import cv2
+
+# initializing the object tracker
+object_tracker = cv2.TrackerMedianFlow.create()
+webcam = cv2.VideoCapture(0)
+ret, frame = webcam.read()
+
+# creating the ROI
+roi = cv2.selectROI("Tracking", frame, False)
+
+    
